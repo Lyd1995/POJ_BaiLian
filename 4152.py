0e5b419cf159c404cb889813e37f63da57a4984c
Ceveraise <842354018@qq.com>
2018-09-05 22:37:50 +0800

    最佳加法表达式
    描述
    给定n个1到9的数字，要求在数字之间摆放m个加号(加号两边必须有数字），使得所得到的加法表达式的值最小，并输出该值。例如，在1234中摆放1个加号，最好的摆法就是12+34,和为36
    输入
    有不超过15组数据
    每组数据两行。第一行是整数m，表示有m个加号要放( 0<=m<=50)
    第二行是若干个数字。数字总数n不超过50,且 m <= n-1
    输出
    对每组数据，输出最小加法表达式的值

---
+++ 4152.py
@@ -0,0 +1,40 @@
+def main():
+    while True:
+        try:
+            m = eval(input())
+            s = input()
+
+            result = best_add(m, s)
+            print(result)
+        except:
+            break
+
+def best_add(m,s):
+    dp=[]
+    dp.append([])
+    dp[0].append("0000")
+    for i in range(1,s.__len__()+1):
+        dp.append([])
+        for j in range(0,m+1):
+            if j==0:
+                dp[i].append(s[0:i])
+                continue
+            if i==j:
+                break
+            min_dp(dp,s,i,j)
+    return dp[i][j]
+
+def min_dp(dp,s,n,m):
+    temp=[0]
+    min=-1
+    for i in range(1,n-m+1):
+        temp.append(int(dp[n-i][m-1])+int(s[n-i:n]))
+    for i in range(1,n-m+1):
+        if min==-1:
+            min=temp[i]
+        else:
+            if min>temp[i]:
+                min=temp[i]
+    dp[n].append(str(min))
+
+main()
